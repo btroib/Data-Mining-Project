4f6c5b015611d72c87c88fc4df8250010373b975
Brian Troib <btroib@gmail.com>
2021-03-06 11:48:11 +0200

    made program lighter

--- web_scraper.py
+++ web_scraper.py
@@ -32,14 +32,11 @@ def main():
 
     if len(sys.argv) < REQUIRED_NUM_OF_ARGS:
         print("ERROR: Insufficient input.")
-        print("usage: /web_scraper.py /output_path #pages Link Rank Title...)")
+        print("usage: /web_scraper.py #pages Link Rank Title...)")
         sys.exit(1)
-
     try:
-        output_path = sys.argv[1]
-        pages_to_scrap = int(sys.argv[2])
-        categories_to_scrap = sys.argv[3:]
-        abs_path = abs_path_creator(output_path)
+        pages_to_scrap = int(sys.argv[1])
+        categories_to_scrap = sys.argv[2:]
         dic_key_list = dic_keys_creator(categories_to_scrap)
         for key in dic_key_list:
             if key not in POSSIBLE_CATEGORIES:
@@ -48,45 +45,37 @@ def main():
                 raise SyntaxError
         dic_values_list = dic_values_creator(dic_key_list)
         dic = dict(zip(dic_key_list, dic_values_list))
-
         headers = {'User-Agent': ''}
         url = 'https://www.metacritic.com/browse/games/score/metascore/all/all/filtered?page=0'
         for i in range(pages_to_scrap):
             page_url = url[0:len(url) - 1] + str(i)
             print(f'Downloading page_{i}...')
             test = requests.get(page_url, headers=headers)
-            outfile = open(abs_path + f'/page_{i}.html', 'w')
-            test.encoding = 'ISO-8859-1'
-            outfile.write(str(test.text))
-            with open(abs_path + f'/page_{i}.html') as html_file:
-                soup = BeautifulSoup(html_file, 'lxml')
-                for game in soup.find_all('td', class_="clamp-summary-wrap"):
-                    if "Link" in dic_key_list:
-                        dic["Link"].append('https://www.metacritic.com' + str(game.find('a', class_="title"))
-                                           .split('\"')[3])
-                    if "Rank" in dic_key_list:
-                        dic["Rank"].append(game.find('span', class_="title numbered").text.split()[0].strip('.'))
-                    if "Title" in dic_key_list:
-                        dic["Title"].append(game.h3.text)
-                    if "Date" in dic_key_list:
-                        dic["Date"].append(' '.join(game.find('div', class_="clamp-details").text.split()[-3:]))
-                    if "Platform" in dic_key_list:
-                        dic["Platform"].append(' '.join(game.find('span', class_="data").text.split()))
-                    if "Meta_Score" in dic_key_list:
-                        dic["Meta_Score"].append(game.find('div', class_="clamp-metascore").text.split()[1])
-                    if "User_Score" in dic_key_list:
-                        dic["User_Score"].append(game.find('div', class_="clamp-userscore").text.split()[2])
-                    if "Game_Summary" in dic_key_list:
-                        dic["Game_Summary"].append(' '.join(game.find('div', class_="summary").text.split()))
+            soup = BeautifulSoup(test.text, 'html.parser')
+            for game in soup.find_all('td', class_="clamp-summary-wrap"):
+                if "Link" in dic_key_list:
+                    dic["Link"].append('https://www.metacritic.com' + str(game.find('a', class_="title")).split('\"')[3])
+                if "Rank" in dic_key_list:
+                    dic["Rank"].append(game.find('span', class_="title numbered").text.split()[0].strip('.'))
+                if "Title" in dic_key_list:
+                    dic["Title"].append(game.h3.text)
+                if "Date" in dic_key_list:
+                    dic["Date"].append(' '.join(game.find('div', class_="clamp-details").text.split()[-3:]))
+                if "Platform" in dic_key_list:
+                    dic["Platform"].append(' '.join(game.find('span', class_="data").text.split()))
+                if "Meta_Score" in dic_key_list:
+                    dic["Meta_Score"].append(game.find('div', class_="clamp-metascore").text.split()[1])
+                if "User_Score" in dic_key_list:
+                    dic["User_Score"].append(game.find('div', class_="clamp-userscore").text.split()[2])
+                if "Game_Summary" in dic_key_list:
+                    dic["Game_Summary"].append(' '.join(game.find('div', class_="summary").text.split()))
         df = pd.DataFrame(dic)
         print(df)
     except SyntaxError:
         sys.exit(1)
-
     except ValueError:
         print('ERROR: Incorrect input. The number of pages must be numerical.')
         sys.exit(1)
-
 
 if __name__ == '__main__':
     main()
