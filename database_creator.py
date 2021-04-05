2366866bdd48a30c80bca7a43ba95955619d26ee
“Andre <andregutnik@gmail.com>
2021-04-05 15:57:11 +0300

    Added exception and log error for wrond MYSQL password

--- database_creator.py
+++ database_creator.py
@@ -1,5 +1,6 @@
 import pymysql.cursors
 import logging
+import sys
 
 # Logging configuration
 logging.basicConfig(filename='web_scraper.log', level=logging.INFO,
@@ -7,34 +8,41 @@ logging.basicConfig(filename='web_scraper.log', level=logging.INFO,
 
 def create(password):
     """Function that creates the metacritic database"""
-    connection = pymysql.connect(host='localhost',
-                                 user='root',
-                                 password=f'{password}',
-                                 cursorclass=pymysql.cursors.DictCursor)
+    try:
+        connection = pymysql.connect(host='localhost',
+                                    user='root',
+                                    password=f'{password}',
+                                    cursorclass=pymysql.cursors.DictCursor)
 
-    with connection.cursor() as cur:
-        cur.execute('DROP DATABASE IF EXISTS metacritic;')
-        cur.execute('CREATE DATABASE metacritic;')
+    except pymysql.err.OperationalError as error:
+        logging.error(f" MYSQL wrong password entered")
+        print("Are you sure you entered the correct password? ")
+        print(error)
+        sys.exit(1)
+    else:
+        with connection.cursor() as cur:
+            cur.execute('DROP DATABASE IF EXISTS metacritic;')
+            cur.execute('CREATE DATABASE metacritic;')
 
-        cur.execute('USE metacritic;')
-        cur.execute("""CREATE TABLE game
-                        (id int PRIMARY KEY,
-                        title varchar(100),
-                        date varchar(100));""")
-        cur.execute("""CREATE TABLE game_score
-                        (game_id int,
-                        ranking int,
-                        meta_score float,
-                        user_score float,
-                        FOREIGN KEY (game_id) REFERENCES game(id));""")
-        cur.execute("""CREATE TABLE platform
-                        (game_id int,
-                        platform varchar(30),
-                        FOREIGN KEY (game_id) REFERENCES game(id));""")
-        cur.execute("""CREATE TABLE link_url
-                        (game_id int,
-                        metacritic_link varchar(255),
-                        youtube_link varchar(255),
-                        FOREIGN KEY (game_id) REFERENCES game(id));""")
-    print('Database successfully created.')
-    logging.info(' Metacritic database successfully created')
+            cur.execute('USE metacritic;')
+            cur.execute("""CREATE TABLE game
+                            (id int PRIMARY KEY,
+                            title varchar(100),
+                            date varchar(100));""")
+            cur.execute("""CREATE TABLE game_score
+                            (game_id int,
+                            ranking int,
+                            meta_score float,
+                            user_score float,
+                            FOREIGN KEY (game_id) REFERENCES game(id));""")
+            cur.execute("""CREATE TABLE platform
+                            (game_id int,
+                            platform varchar(30),
+                            FOREIGN KEY (game_id) REFERENCES game(id));""")
+            cur.execute("""CREATE TABLE link_url
+                            (game_id int,
+                            metacritic_link varchar(255),
+                            youtube_link varchar(255),
+                            FOREIGN KEY (game_id) REFERENCES game(id));""")
+        print('Database successfully created.')
+        logging.info(' Metacritic database successfully created')
